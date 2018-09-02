64fdf063b5143bd8e5625d8d589464056905c092
Kouhei Sutou <kou@clear-code.com>
2018-09-02 22:50:28 +0900

    Make REXML::REVISION always ""
    It uses a Subversion feature.
    It's meaningless now.

--- lib/rexml/rexml.rb
+++ lib/rexml/rexml.rb
@@ -25,7 +25,7 @@ module REXML
   COPYRIGHT = "Copyright © 2001-2008 Sean Russell <ser@germane-software.com>"
   DATE = "2008/019"
   VERSION = "3.1.7.3"
-  REVISION = %w$Revision$[1] || ''
+  REVISION = ""
 
   Copyright = COPYRIGHT
   Version = VERSION
