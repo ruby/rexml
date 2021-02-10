b2a7a8ad0b095fd65c320826f21c848bd022fa54
BurdetteLamar <burdettelamar@yahoo.com>
2021-02-10 07:47:39 -0600

    Adding method Elements#parent

--- lib/rexml/element.rb
+++ lib/rexml/element.rb
@@ -956,26 +956,22 @@ module REXML
   class Elements
     include Enumerable
     # :call-seq:
-    #   new(base_element) -> new_elements_object
+    #   new(parent) -> new_elements_object
     #
-    # Returns a new \Elements object with the given +base_element+.
-    # Does _not_ assign <tt>base_element.elements = self</tt>:
+    # Returns a new \Elements object with the given +parent+.
+    # Does _not_ assign <tt>parent.elements = self</tt>:
     #
     #   d = REXML::Document.new(xml_string)
     #   eles = REXML::Elements.new(d.root)
     #   eles # => #<REXML::Elements @element=<bookstore> ... </>>
     #   eles == d.root.elements # => false
     #
-    # To retrieve the given +base_element+:
-    #
-    #   eles['.'] # => <bookstore> ... </>
-    #
     def initialize parent
       @element = parent
     end
 
     # :call-seq:
-    #   base_element
+    #   parent
     #
     # Returns the base element cited in creating the \Elements object.
     # This element is also the default base, starting point, for searching
@@ -983,7 +979,7 @@ module REXML
     #
     #   d = REXML::Document.new(xml_string)
     #   elements = REXML::Elements.new(d.root)
-    #   elements.base_element == d.root # => true
+    #   elements.parent == d.root # => true
     #
     def parent
       @element
