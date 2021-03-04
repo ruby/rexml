e03baffffcbd469fe5ca252f59c237947d044275
BurdetteLamar <burdettelamar@yahoo.com>
2021-03-04 09:18:04 -0600

    Doc for specific tasks

--- doc/rexml/tasks/mktocs.rb
+++ doc/rexml/tasks/mktocs.rb
@@ -1,35 +1,35 @@
 Dir.chdir(File.dirname(__FILE__)) do
 
-  system('rm -fr html')
-  system('rdoc --op html rdoc')
-  require 'fileutils'
-  include FileUtils
-  FileUtils.mkdir_p('tocs')
-
+  # Get the correct links from actual (temporary) rdoc html.
   toc_lis_by_name = {}
-  Dir.chdir('html/rdoc') do
-    Dir.new('.').entries.each do |html_file_path|
-      next if html_file_path.start_with?('.')
-      toc_lis = []
-      File.open(html_file_path, 'r') do |file|
-        in_toc = false
-        file.each_line do |line|
-          unless in_toc
-            if line.include?('<ul class="link-list" role="directory">')
-              in_toc = true
-              next
+  require 'tmpdir'
+  Dir.mktmpdir do |tmpdir|
+    system("rdoc --op #{tmpdir} --force-output rdoc")
+    Dir.chdir("#{tmpdir}/rdoc") do
+      Dir.new('.').entries.each do |html_file_path|
+        next if html_file_path.start_with?('.')
+        toc_lis = []
+        File.open(html_file_path, 'r') do |file|
+          in_toc = false
+          file.each_line do |line|
+            unless in_toc
+              if line.include?('<ul class="link-list" role="directory">')
+                in_toc = true
+                next
+              end
+            end
+            if in_toc
+              break if line.include?('</ul>')
+              toc_lis.push(line.chomp)
             end
           end
-          if in_toc
-            break if line.include?('</ul>')
-            toc_lis.push(line.chomp)
-          end
         end
+        key = html_file_path.sub('_rdoc.html', '')
+        toc_lis_by_name[key] = toc_lis
       end
-      key = html_file_path.sub('_rdoc.html', '')
-      toc_lis_by_name[key] = toc_lis
     end
   end
+
   File.open('tocs/master_toc.rdoc', 'w') do |master_toc_file|
     master_toc_file.write("== Table of Contents\n\n")
     Dir.chdir('tocs') do
@@ -45,12 +45,12 @@ Dir.chdir(File.dirname(__FILE__)) do
             toc_entry = "#{indentation}- {#{text}}[#{link}]\n"
             if i == 0
               text = text.split(' ')[1]
-              link = "tasks/#{text.downcase}_rdoc.html"
+              link = "../../tasks/rdoc/#{text.downcase}_rdoc.html"
+              p link
               master_toc_file.write("=== {#{text}}[#{link}]\n")
-              # master_toc_file.write("=== #{text}\n\n")
               next
             end
-            master_link = "tasks/#{toc_file_name.sub('_toc.rdoc', '_rdoc.html')}#{link}"
+            master_link = "../../tasks/rdoc/#{toc_file_name.sub('_toc.rdoc', '_rdoc.html')}#{link}"
             master_toc_entry = "#{indentation}- {#{text}}[#{master_link}]\n"
             master_toc_file.write(master_toc_entry)
             class_file.write(toc_entry)
@@ -61,6 +61,5 @@ Dir.chdir(File.dirname(__FILE__)) do
       end
     end
   end
-  system('rm -fr html')
 
 end
