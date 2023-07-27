10c9cfea11b2bde3e3c0096cadcd03522c0d1ed7
Sutou Kouhei <kou@clear-code.com>
2023-07-27 17:11:51 +0900

    Bump version

--- lib/rexml/rexml.rb
+++ lib/rexml/rexml.rb
@@ -31,7 +31,7 @@
 module REXML
   COPYRIGHT = "Copyright © 2001-2008 Sean Russell <ser@germane-software.com>"
   DATE = "2008/019"
-  VERSION = "3.2.6"
+  VERSION = "3.2.7"
   REVISION = ""
 
   Copyright = COPYRIGHT
