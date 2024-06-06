da67561afb2a5f6910c69d5e0e73bea8d457f303
Sutou Kouhei <kou@clear-code.com>
2024-06-06 10:54:13 +0900

    test: reduce the number of rehearsal executions
    It reduces test execution time.

--- test/test_document.rb
+++ test/test_document.rb
@@ -204,7 +204,7 @@ EOX
 
     def test_gt_linear_performance_attribute_value
       seq = [10000, 50000, 100000, 150000, 200000]
-      assert_linear_performance(seq) do |n|
+      assert_linear_performance(seq, rehearsal: 10) do |n|
         REXML::Document.new('<test testing="' + ">" * n + '"></test>')
       end
     end
