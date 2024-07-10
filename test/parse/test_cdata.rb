f0c591ffd4156b178a7a3c9331d3a4f77e764cac
Watson <watson1978@gmail.com>
2024-07-10 10:56:22 +0900

    Fix performance issue caused by using repeated `>` characters inside `CDATA [ PAYLOAD ]`
    A `<` is treated as a string delimiter. 
    In certain cases, if `<` is used in succession, read and match are
    repeated, which slows down the process. Therefore, the following is used
    to read ahead to a specific part of the string in advance.

---
+++ test/parse/test_cdata.rb
@@ -0,0 +1,17 @@
+require "test/unit"
+require "core_assertions"
+
+require "rexml/document"
+
+module REXMLTests
+  class TestParseCData < Test::Unit::TestCase
+    include Test::Unit::CoreAssertions
+
+    def test_gt_linear_performance_cdata
+      seq = [10000, 50000, 100000, 150000, 200000]
+      assert_linear_performance(seq, rehearsal: 10) do |n|
+        REXML::Document.new('<description><![CDATA[ ' + ">" * n + ' ]]></description>')
+      end
+    end
+  end
+end
