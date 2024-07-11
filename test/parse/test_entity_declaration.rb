c8fa85aded50f015e4bd513f6e9edfa97f2d5f84
Watson <watson1978@gmail.com>
2024-07-11 14:32:27 +0900

    Fix performance issue caused by using repeated `>` characters inside `<!DOCTYPE name [<!ENTITY>]>`
    A `<` is treated as a string delimiter. 
    In certain cases, if `<` is used in succession, read and match are
    repeated, which slows down the process. Therefore, the following is used
    to read ahead to a specific part of the string in advance.

--- test/parse/test_entity_declaration.rb
+++ test/parse/test_entity_declaration.rb
@@ -32,5 +32,12 @@ Last 80 unconsumed characters:
  <!ENTITY>  ]> <r/>
       DETAIL
     end
+
+    def test_gt_linear_performance_entity
+      seq = [10000, 50000, 100000, 150000, 200000]
+      assert_linear_performance(seq, rehearsal: 10) do |n|
+        REXML::Document.new('<!DOCTYPE rubynet [<!ENTITY rbconfig.ruby_version "' + '>' * n + '">')
+      end
+    end
   end
 end
