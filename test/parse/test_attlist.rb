3819f9fe80279f40639f4808bf257340548797fa
Watson <watson1978@gmail.com>
2024-07-11 14:54:06 +0900

    Fix ReDoS by using repeated space characters inside `<!DOCTYPE name [<!ATTLIST>]>`
    Fix performance by removing unnecessary spaces.

---
+++ test/parse/test_attlist.rb
@@ -0,0 +1,17 @@
+require "test/unit"
+require "core_assertions"
+
+require "rexml/document"
+
+module REXMLTests
+  class TestParseAttlist < Test::Unit::TestCase
+    include Test::Unit::CoreAssertions
+
+    def test_gt_linear_performance_attlist
+      seq = [10000, 50000, 100000, 150000, 200000]
+      assert_linear_performance(seq, rehearsal: 10) do |n|
+        REXML::Document.new('<!DOCTYPE schema SYSTEM "foo.dtd" [<!ATTLIST ' + " " * n + ' root v CDATA #FIXED "test">]>')
+      end
+    end
+  end
+end
