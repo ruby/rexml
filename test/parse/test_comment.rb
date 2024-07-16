17fd8e8bd5ac19b95e3f6327cb42e491676f6c58
Sutou Kouhei <kou@cozmixng.org>
2024-07-16 11:31:45 +0900

    Remove redundant "comment"

--- test/parse/test_comment.rb
+++ test/parse/test_comment.rb
@@ -129,7 +129,7 @@ module REXMLTests
       end
     end
 
-    def test_gt_linear_performance_comment_in_element
+    def test_gt_linear_performance_in_element
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
         REXML::Document.new('<xml><!-- ' + '>' * n + ' --></xml>')
