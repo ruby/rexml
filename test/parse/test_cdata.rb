ce43daee5cba24dd08e33bc5d5e1b0bf817e772c
Sutou Kouhei <kou@cozmixng.org>
2024-07-16 11:00:46 +0900

    Remove redundatnt "cdata"

--- test/parse/test_cdata.rb
+++ test/parse/test_cdata.rb
@@ -7,7 +7,7 @@ module REXMLTests
   class TestParseCData < Test::Unit::TestCase
     include Test::Unit::CoreAssertions
 
-    def test_gt_linear_performance_cdata
+    def test_gt_linear_performance
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
         REXML::Document.new('<description><![CDATA[ ' + ">" * n + ' ]]></description>')
