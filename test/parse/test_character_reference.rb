a2a8fb114262a5a6ff03102a7cda34dca60415b5
Watson <watson1978@gmail.com>
2024-07-16 10:49:19 +0900

    Update test

--- test/parse/test_character_reference.rb
+++ test/parse/test_character_reference.rb
@@ -7,13 +7,10 @@ module REXMLTests
   class TestParseCharacterReference < Test::Unit::TestCase
     include Test::Unit::CoreAssertions
 
-    def test_gt_linear_performance_malformed_character_reference
+    def test_gt_linear_performance_character_reference
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
-        begin
-          REXML::Document.new('<test testing="&#' + "0" * n + '"/>')
-        rescue
-        end
+        REXML::Document.new('<test testing="&#' + "0" * n + '97;"/>')
       end
     end
   end
