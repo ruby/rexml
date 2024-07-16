c1b64c174ec2e8ca2174c51332670e3be30c865f
Watson <watson1978@gmail.com>
2024-07-16 10:57:50 +0900

    Fix performance issue caused by using repeated `>` characters inside comments (#171)
    A `<` is treated as a string delimiter. 
    In certain cases, if `<` is used in succession, read and match are
    repeated, which slows down the process. Therefore, the following is used
    to read ahead to a specific part of the string in advance.

--- test/parse/test_comment.rb
+++ test/parse/test_comment.rb
@@ -1,8 +1,12 @@
 require "test/unit"
+require "core_assertions"
+
 require "rexml/document"
 
 module REXMLTests
   class TestParseComment < Test::Unit::TestCase
+    include Test::Unit::CoreAssertions
+
     def parse(xml)
       REXML::Document.new(xml)
     end
@@ -116,6 +120,13 @@ module REXMLTests
       end
 
       assert_equal(" ok comment ", events[:comment])
+    end
+
+    def test_gt_linear_performance
+      seq = [10000, 50000, 100000, 150000, 200000]
+      assert_linear_performance(seq, rehearsal: 10) do |n|
+        REXML::Document.new('<!-- ' + ">" * n + ' -->')
+      end
     end
   end
 end
