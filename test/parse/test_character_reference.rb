6d33f1dbe14e3d4dfaf087ca608e4eccfe70f47e
Sutou Kouhei <kou@cozmixng.org>
2024-07-16 10:52:03 +0900

    Improve test name

--- test/parse/test_character_reference.rb
+++ test/parse/test_character_reference.rb
@@ -7,7 +7,7 @@ module REXMLTests
   class TestParseCharacterReference < Test::Unit::TestCase
     include Test::Unit::CoreAssertions
 
-    def test_gt_linear_performance_character_reference
+    def test_gt_linear_performance_many_preceding_zeros
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
         REXML::Document.new('<test testing="&#' + "0" * n + '97;"/>')
