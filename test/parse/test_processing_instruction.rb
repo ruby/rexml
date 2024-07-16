18a27fea3c990342bb79adf4b81f40b0cc187d46
Sutou Kouhei <kou@cozmixng.org>
2024-07-16 10:43:12 +0900

    Remove redundant test name

--- test/parse/test_processing_instruction.rb
+++ test/parse/test_processing_instruction.rb
@@ -74,7 +74,7 @@ x<?x y
       assert_equal("abc", events[:processing_instruction])
     end
 
-    def test_gt_linear_performance_introduction
+    def test_gt_linear_performance
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
         REXML::Document.new('<?xml version="1.0" ' + ">" * n + ' ?>')
