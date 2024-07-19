f946711cf7434ed994c26eb0ebe19981cfc6304b
Watson <watson1978@gmail.com>
2024-07-19 12:35:04 +0900

    Fix method scope in test in order to invoke the tests properly
    The `test_empty` and `test_linear_performance_gt` were defined as private method.
    Seems that test-unit runner does not invoke private methods even if the methods have `test_` prefix.

--- test/parse/test_entity_declaration.rb
+++ test/parse/test_entity_declaration.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: false
-require 'test/unit'
-require 'rexml/document'
+require "test/unit"
+require "core_assertions"
+
+require "rexml/document"
 
 module REXMLTests
   class TestParseEntityDeclaration < Test::Unit::TestCase
+    include Test::Unit::CoreAssertions
+
     private
     def xml(internal_subset)
       <<-XML
@@ -18,25 +22,22 @@ module REXMLTests
       REXML::Document.new(xml(internal_subset)).doctype
     end
 
+    public
     def test_empty
-      exception = assert_raise(REXML::ParseException) do
+      assert_raise(REXML::ParseException) do
         parse(<<-INTERNAL_SUBSET)
 <!ENTITY>
         INTERNAL_SUBSET
       end
-      assert_equal(<<-DETAIL.chomp, exception.to_s)
-Malformed notation declaration: name is missing
-Line: 5
-Position: 72
-Last 80 unconsumed characters:
- <!ENTITY>  ]> <r/>
-      DETAIL
     end
 
     def test_linear_performance_gt
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
-        REXML::Document.new('<!DOCTYPE rubynet [<!ENTITY rbconfig.ruby_version "' + '>' * n + '">')
+        begin
+          REXML::Document.new('<!DOCTYPE rubynet [<!ENTITY rbconfig.ruby_version "' + '>' * n + '">')
+        rescue
+        end
       end
     end
   end
