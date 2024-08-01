be86b3de0aca8394534b715a83a63bf51c5195f5
Sutou Kouhei <kou@clear-code.com>
2024-08-01 11:35:05 +0900

    test: fix wrong test name

--- test/parse/test_attribute_list_declaration.rb
+++ test/parse/test_attribute_list_declaration.rb
@@ -7,7 +7,7 @@ module REXMLTests
   class TestParseAttributeListDeclaration < Test::Unit::TestCase
     include Test::Unit::CoreAssertions
 
-    def test_linear_performance_gt
+    def test_linear_performance_space
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
         REXML::Document.new("<!DOCTYPE schema SYSTEM \"foo.dtd\" [<!ATTLIST " +
