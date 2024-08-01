163d366f21a6d66bf7104f2283eac5b07676c5f8
Sutou Kouhei <kou@clear-code.com>
2024-08-01 09:52:48 +0900

    test: use double quote for string literal

--- test/parse/test_document_type_declaration.rb
+++ test/parse/test_document_type_declaration.rb
@@ -294,7 +294,7 @@ x'>  <r/>
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
         begin
-          REXML::Document.new('<!DOCTYPE root [' + "%>" * n + ']><test/>')
+          REXML::Document.new("<!DOCTYPE root [" + "%>" * n + "]><test/>")
         rescue
         end
       end
@@ -303,7 +303,7 @@ x'>  <r/>
     def test_linear_performance_comment_gt
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
-        REXML::Document.new('<!DOCTYPE root [<!-- ' + ">" * n + ' -->]>')
+        REXML::Document.new("<!DOCTYPE root [<!-- " + ">" * n + " -->]>")
       end
     end
   end
