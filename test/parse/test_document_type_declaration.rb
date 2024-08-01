50c725249e434ae89d6286827368af6d0ccea146
Watson <watson1978@gmail.com>
2024-08-01 09:56:36 +0900

    test: add a performance test for %...; in document declaration

--- test/parse/test_document_type_declaration.rb
+++ test/parse/test_document_type_declaration.rb
@@ -306,5 +306,12 @@ x'>  <r/>
         REXML::Document.new("<!DOCTYPE root [<!-- " + ">" * n + " -->]>")
       end
     end
+
+    def test_linear_performance_external_entity_right_bracket_gt
+      seq = [10000, 50000, 100000, 150000, 200000]
+      assert_linear_performance(seq, rehearsal: 10) do |n|
+        REXML::Document.new("<!DOCTYPE root [%" + "]>" * n + ";]>")
+      end
+    end
   end
 end
