17ff3e78745b31db4e841357e8eed2f6669bea7b
Watson <watson1978@gmail.com>
2024-08-01 11:45:51 +0900

    test: add a performance test for attribute list declaration

--- test/parse/test_attribute_list_declaration.rb
+++ test/parse/test_attribute_list_declaration.rb
@@ -15,5 +15,16 @@ module REXMLTests
                             " root v CDATA #FIXED \"test\">]>")
       end
     end
+
+    def test_linear_performance_tab_and_gt
+      seq = [10000, 50000, 100000, 150000, 200000]
+      assert_linear_performance(seq, rehearsal: 10) do |n|
+        REXML::Document.new("<!DOCTYPE root [<!ATTLIST " +
+                            "\t" * n +
+                            "root value CDATA \"" +
+                            ">" * n +
+                            "\">]>")
+      end
+    end
   end
 end
