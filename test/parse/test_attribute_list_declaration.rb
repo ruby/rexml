b93d790b36c065a3f7f3e0c3f5b2b71254a4d96d
Sutou Kouhei <kou@clear-code.com>
2024-08-01 11:34:44 +0900

    test: use double quote for string literal

--- test/parse/test_attribute_list_declaration.rb
+++ test/parse/test_attribute_list_declaration.rb
@@ -10,7 +10,9 @@ module REXMLTests
     def test_linear_performance_gt
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
-        REXML::Document.new('<!DOCTYPE schema SYSTEM "foo.dtd" [<!ATTLIST ' + " " * n + ' root v CDATA #FIXED "test">]>')
+        REXML::Document.new("<!DOCTYPE schema SYSTEM \"foo.dtd\" [<!ATTLIST " +
+                            " " * n +
+                            " root v CDATA #FIXED \"test\">]>")
       end
     end
   end
