1ec9be15f150544a13377827636abb4f52bb0ba7
NAITOH Jun <naitoh@gmail.com>
2025-03-01 10:32:33 +0900

    Improve CDATA parse performance
    ## Why?
    
    GitHub: fix #243
    
    ## Benchmark (Comparison with rexml 3.4.1)
    ```
    $ benchmark-driver benchmark/parse_cdata.yaml
    Calculating -------------------------------------
                         rexml 3.4.1      master  3.4.1(YJIT)  master(YJIT)
                     dom     648.361      1.178k      591.590        1.046k i/s -     100.000 times in 0.154235s 0.084913s 0.169036s 0.095627s
                     sax     699.061      1.378k      651.148        1.196k i/s -     100.000 times in 0.143049s 0.072549s 0.153575s 0.083611s
                    pull     699.271      1.379k      660.275        1.210k i/s -     100.000 times in 0.143006s 0.072527s 0.151452s 0.082622s
                  stream     701.725      1.383k      659.483        1.228k i/s -     100.000 times in 0.142506s 0.072307s 0.151634s 0.081455s
    
    Comparison:
                                  dom
                  master:      1177.7 i/s
            master(YJIT):      1045.7 i/s - 1.13x  slower
             rexml 3.4.1:       648.4 i/s - 1.82x  slower
             3.4.1(YJIT):       591.6 i/s - 1.99x  slower
    
                                  sax
                  master:      1378.4 i/s
            master(YJIT):      1196.0 i/s - 1.15x  slower
             rexml 3.4.1:       699.1 i/s - 1.97x  slower
             3.4.1(YJIT):       651.1 i/s - 2.12x  slower
    
                                 pull
                  master:      1378.8 i/s
            master(YJIT):      1210.3 i/s - 1.14x  slower
             rexml 3.4.1:       699.3 i/s - 1.97x  slower
             3.4.1(YJIT):       660.3 i/s - 2.09x  slower
    
                               stream
                  master:      1383.0 i/s
            master(YJIT):      1227.7 i/s - 1.13x  slower
             rexml 3.4.1:       701.7 i/s - 1.97x  slower
             3.4.1(YJIT):       659.5 i/s - 2.10x  slower
    ```
    - YJIT=ON : 1.76x - 1.83x faster
    - YJIT=OFF : 1.82x - 1.97x faster
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- test/parse/test_cdata.rb
+++ test/parse/test_cdata.rb
@@ -7,10 +7,28 @@ module REXMLTests
   class TestParseCData < Test::Unit::TestCase
     include Test::Unit::CoreAssertions
 
+    def parse(xml)
+      REXML::Document.new(xml)
+    end
+
     def test_linear_performance_gt
       seq = [10000, 50000, 100000, 150000, 200000]
       assert_linear_performance(seq, rehearsal: 10) do |n|
-        REXML::Document.new('<description><![CDATA[ ' + ">" * n + ' ]]></description>')
+        parse('<description><![CDATA[ ' + ">" * n + ' ]]></description>')
+      end
+    end
+
+    class TestInvalid < self
+      def test_unclosed_cdata
+        exception = assert_raise(REXML::ParseException) do
+          parse("<root><![CDATA[a]></root>")
+        end
+        assert_equal(<<~DETAIL, exception.to_s)
+          Malformed CDATA: Missing end ']]>'
+          Line: 1
+          Position: 25
+          Last 80 unconsumed characters:
+        DETAIL
       end
     end
   end
