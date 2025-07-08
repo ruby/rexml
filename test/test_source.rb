548172637b8eb106ea38f3b91f54d0fc2e6e8e08
Sutou Kouhei <kou@clear-code.com>
2025-07-09 06:14:08 +0900

    Don't call needless encoding_updated (#259)
    Needless encoding_updated call may have performance penalty a bit.

--- test/test_source.rb
+++ test/test_source.rb
@@ -12,6 +12,21 @@ module REXMLTests
         assert_equal("UTF-8", @source.encoding)
       end
 
+      test("encoding_updated") do
+        def @source.n_encoding_updated_called
+          @n_encoding_updated_called
+        end
+        def @source.encoding_updated
+          super
+          @n_encoding_updated_called ||= 0
+          @n_encoding_updated_called += 1
+        end
+        @source.encoding = "shift-jis"
+        assert_equal(1, @source.n_encoding_updated_called)
+        @source.encoding = "Shift-JIS"
+        assert_equal(1, @source.n_encoding_updated_called)
+      end
+
       test("Encoding") do
         @source.encoding = Encoding::UTF_8
         assert_equal("UTF-8", @source.encoding)
