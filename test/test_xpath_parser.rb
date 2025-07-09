0be1869bb16fc55642b723428b9b026c4f00b3f9
Peter H. Boling <peter.boling@gmail.com>
2025-07-09 00:57:46 +0000

    🔥 Remove redundant tests

--- test/test_xpath_parser.rb
+++ test/test_xpath_parser.rb
@@ -4,8 +4,8 @@ module REXMLTests
   class TestXPathParser < Test::Unit::TestCase
     def setup
       @root_element = make_service_element(["urn:type1", "urn:type2"], ["http://uri"])
-      @root = @root_element.root
       @element = @root_element.children[0]
+      @parser = REXML::XPathParser.new
     end
 
     def make_service_element(types, uris)
@@ -20,34 +20,7 @@ module REXMLTests
       root_element
     end
 
-    def test_is_first_child
-      assert_kind_of(REXML::Element, @element)
-    end
-
-    def test_has_element_as_parent
-      assert_kind_of(REXML::Element, @element.parent)
-    end
-
-    def test_has_element_as_root
-      assert_kind_of(REXML::Element, @root)
-    end
-
-    def test_parent_is_root
-      assert_equal(@root, @element.parent)
-    end
-
-    def test_has_nil_siblings
-      assert_nil(@root.previous_sibling)
-      assert_nil(@element.next_sibling)
-    end
-
-    def test_has_not_nil_siblings
-      assert_kind_of(REXML::Element, @element.children[0].next_sibling)
-      assert_kind_of(REXML::Element, @element.children[1].previous_sibling)
-    end
-
     def test_found
-      @parser = REXML::XPathParser.new
       res = @parser.parse("/Service", @root_element)
       assert_equal([@element],
                    res)
