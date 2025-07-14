393908377ed55276df9079dc3b11b8cad62a9ee5
|7eter l-|. l3oling <peter.boling@gmail.com>
2025-07-14 17:18:49 +0700

    Update test/test_text.rb
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- test/test_text.rb
+++ test/test_text.rb
@@ -72,7 +72,9 @@ module REXMLTests
 
     def test_indent_text
       text = Text.new("")
-      assert_equal("\tline1\tline2\tline3", text.indent_text("line1\r\nline2\r\nline3\r\n"))
+      suppress_warning do
+        assert_equal("\tline1\tline2\tline3", text.indent_text("line1\r\nline2\r\nline3\r\n"))
+      end
     end
   end
 end
