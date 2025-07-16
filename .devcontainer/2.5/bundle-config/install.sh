5f5834070c3bd9fe066258503ff0f826c0b4d83f
Peter H. Boling <peter.boling@gmail.com>
2025-07-16 03:00:33 +0000

    🔥 Copy fresh Gemfile for Ruby 2.5 each time devcontainer is run

--- .devcontainer/2.5/bundle-config/install.sh
+++ .devcontainer/2.5/bundle-config/install.sh
@@ -2,5 +2,8 @@
 RUBYGEMS_VERSION=3.3.27
 gem install rubygems-update -v ${RUBYGEMS_VERSION} > /dev/null 2>&1 &&
 update_rubygems > /dev/null 2>&1
+# Depends on making a copy of the Gemfile, so we don't conflict with the Gemfile.lock
+#   that is probably relegated to newer Ruby.
+# We need to set the ENV variable here, but must wait to copy the Gemfile until the postCreateCommand hook.
 # Ensure we use the right Gemfile
 echo 'export BUNDLE_GEMFILE=Gemfile-ruby2_5.gemfile' >> ~/.bashrc
