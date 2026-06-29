02c664dd5d97b2ef01661d5932a852ae0c4e5500
John Hawthorn <john@hawthorn.email>
2026-06-28 19:43:18 -0700

    Make rexml/element.rb frozen_string_literal: true

--- lib/rexml/element.rb
+++ lib/rexml/element.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 require_relative "parent"
 require_relative "namespace"
 require_relative "attribute"
@@ -1524,7 +1524,7 @@ module REXML
     def _namespace_lookup_internal(prefix, namespaces = _calculate_namespaces) # :nodoc:
       prefix = (prefix == '') ? 'xmlns' : prefix.delete_prefix("xmlns:")
       ns = namespaces[prefix]
-      ns = '' if ns.nil? and prefix == 'xmlns'
+      ns = +'' if ns.nil? and prefix == 'xmlns'
       ns
     end
 
